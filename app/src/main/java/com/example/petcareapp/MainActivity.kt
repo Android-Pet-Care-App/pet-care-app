d808286b84e0e8012b7ed50d0aa2c87fa47b792d
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2024-04-01 17:43:14 -0400

    have bottom navigation

--- app/src/main/java/com/example/petcareapp/MainActivity.kt
+++ app/src/main/java/com/example/petcareapp/MainActivity.kt
@@ -1,46 +1,102 @@
 package com.example.petcareapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.petcareapp.ui.theme.PetCareAppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PetCareAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                AppContent()
             }
         }
     }
 }
 
 @Composable
+fun AppContent() {
+    Scaffold(
+        bottomBar = { BottomNavigationBar() }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column {
+            Greeting("Android")
+            Greeting("BOB")
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar() {
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf("Profile","Home", "Label")
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        if (index==0) Icons.Filled.Person
+                        else if (index==1) Icons.Filled.Home
+                        else Icons.Filled.Face,
+                        contentDescription = item
+                    )},
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    // Add your action here when a navigation item is selected
+                }
+            )
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Text(text = "Hello $name!", modifier = modifier.padding(24.dp), color = Color.Black)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PetCareAppTheme {
-        Greeting("Android")
+        AppContent()
     }
 }
