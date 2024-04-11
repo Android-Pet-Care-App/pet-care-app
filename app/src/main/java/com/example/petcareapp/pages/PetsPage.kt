bc9cd8bf5d43b481095f166bc3b512d0709b93c3
saioku <oku.saipranith@gmail.com>
2024-04-11 12:20:55 -0400

    code for pets page

--- app/src/main/java/com/example/petcareapp/pages/PetsPage.kt
+++ app/src/main/java/com/example/petcareapp/pages/PetsPage.kt
@@ -1,5 +1,9 @@
 package com.example.petcareapp
 
+//import androidx.compose.material.Button
+//import androidx.compose.material.ButtonDefaults
+//import androidx.compose.material.MaterialTheme
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -13,26 +17,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-
-//import androidx.compose.material.Button
-//import androidx.compose.material.ButtonDefaults
-//import androidx.compose.material.MaterialTheme
-
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+/*code for the pets page*/
 
 @Composable
 fun PetsPage(title: String) {
