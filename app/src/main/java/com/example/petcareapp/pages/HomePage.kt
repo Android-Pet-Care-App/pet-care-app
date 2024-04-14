6360bc3761517a7c029de7f9ce06235497ba5670
Michael Hernando <mhernando334@gmail.com>
2024-04-14 12:42:58 -0400

    Button and title creation. Initialization of Task component.

--- app/src/main/java/com/example/petcareapp/pages/HomePage.kt
+++ app/src/main/java/com/example/petcareapp/pages/HomePage.kt
@@ -3,6 +3,10 @@ package com.example.petcareapp
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,6 +14,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -20,9 +30,32 @@ fun HomePage() {
         return
     }
 
-    Column {
-        Button(onClick = { showCreateTask = true }) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp) // Add margin around the Column
+    ) {
+        Text(
+            text = "Today's Tasks",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = { showCreateTask = true },
+            modifier = Modifier
+                .fillMaxWidth() // Button takes full width of parent
+                .padding(horizontal = 20.dp) // Horizontal padding of 20.sp
+                .align(Alignment.End) // Align button to the end (bottom) of the column
+        ) {
             Text(text = "Create Task +")
         }
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview
+@Composable
+fun HomePagePreview() {
+    HomePage() // Simply call your composable function within the preview function
 }
